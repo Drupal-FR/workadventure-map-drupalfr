9e1f42b0513e5a65b937399551329fe7eb30f0e9
Florent Torregrosa <florent.torregrosa@gmail.com>
2022-02-08 21:47:02 +0100

    Remove tilesets where free usage is not verified. Fix multiple popups closing.

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,23 @@ import {bootstrapExtra} from "@workadventure/scripting-api-extra";
 // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure.
 bootstrapExtra().catch(e => console.error(e));
 
-let currentPopup: any = undefined;
-
 // Welcome.
+let welcomePopup: any = undefined;
 WA.room.onEnterLayer('welcomeZone').subscribe(() => {
-    currentPopup = WA.ui.openPopup("welcomeMessage","Bienvenu(e) sur la carte Workadventure de l'association Drupal France !",[]);
+    welcomePopup = WA.ui.openPopup("welcomeMessage","Bienvenu(e) sur la carte Workadventure de l'association Drupal France !",[]);
 })
-WA.room.onLeaveLayer('welcomeZone').subscribe(closePopUp)
+WA.room.onLeaveLayer('welcomeZone').subscribe(closePopUp(welcomePopup))
 
 // Clock.
+let clockPopup: any = undefined;
 WA.room.onEnterLayer('clockZone').subscribe(() => {
     const today = new Date();
     const time = today.getHours() + "h" + today.getMinutes();
-    currentPopup = WA.ui.openPopup("clockPopup","Il est " + time,[]);
+    clockPopup = WA.ui.openPopup("clockPopup","Il est " + time,[]);
 })
-WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
+WA.room.onLeaveLayer('clockZone').subscribe(closePopUp(clockPopup))
 
-function closePopUp(){
+function closePopUp(currentPopup){
     if (currentPopup !== undefined) {
         currentPopup.close();
         currentPopup = undefined;
